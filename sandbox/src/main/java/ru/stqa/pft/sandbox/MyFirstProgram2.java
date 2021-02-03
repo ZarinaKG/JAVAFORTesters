39dd31ccbfca7b7a130e6c08bc977a73bc38e66b
Karymbaeva <zka@compart.com>
2021-02-03 22:58:10 +0100

    Some methods are added.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram2.java
@@ -3,7 +3,19 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram2 {
 	
 	public static  void main(String[] args){
-			System.out.println("Hello, world!");
+			double l=5.0;
+			double a = 10;
+			double b = 25;
+			System.out.println("Ploshad kvadrata:"+l+ " Ploshad:" +area(l));
+			System.out.println("Ploshad prjamougolnika so storonami "+a+ "i "+b+" Ploshad: " +area(a,b));
+	}
+
+	public static double area(double len){
+		return len*len;
+	}
+
+	public static double area(double a, double b){
+		return a*b;
 	}
 
 }
