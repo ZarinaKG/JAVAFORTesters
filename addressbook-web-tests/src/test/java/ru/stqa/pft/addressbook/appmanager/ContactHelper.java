9433c0ca53c32d0443e85ab1d78dd2a29cd6fadb
Karymbaeva <zka@compart.com>
2021-03-01 17:23:55 +0100

    Run the tests with the FIREFOX

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -12,7 +13,7 @@ import static java.lang.Thread.sleep;
 
 public class ContactHelper extends BaseHelper {
 
-  public ContactHelper(FirefoxDriver wd) {
+  public ContactHelper(WebDriver wd) {
     super(wd);
   }
 
